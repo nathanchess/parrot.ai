841b5a029e9251004b113dbd45849aa1363dd546
Green-Kiwie <kierann.schong@gmail.com>
2025-05-10 23:18:22 -0700

    updated json processing to correspond to new schema

--- vector_embedding/processor.py
+++ vector_embedding/processor.py
@@ -8,29 +8,9 @@ import modules.text_segmentation as ts
 import modules.database_interactor as db
 import modules.s3_interactor as s3
 import time
-# #testing text embedding
-# text = "hi this is some sample text"
-
-# print(te.embed_text(text))
-
-
-# #testing aws text segmentation
-# test = "hi this is a sentence. this is another sentence. nathan is the coolest guy around"
-# print('\n\n\n', ts.text_segmentation(test))
-
-# # testing database inserter
-# sample_text = "i hate strawberries"
-# test_embedding = [(sample_text, te.embed_text(sample_text), "test_user", "nathan")]
-# db.batch_upload_embeddings(test_embedding)
-
-# # testing database query
-# query_text = "i love nathan"
-# similar_data = db.similarity_search(te.embed_text(query_text))
-# for x in similar_data:
-#     print(x)
 
 def _process_file_contents(file_contents: dict) -> None:
-    transcript = file_contents["results"]["transcripts"][0]["transcript"]
+    transcript = file_contents["transcript"]
     sentences = ts.text_segmentation(transcript)
 
     upload_contents = []
