6fe5630bc93b3e7aa41d0f81c163602f74ab58d4
Benson Manzano <bensonfmanzano@gmail.com>
2025-05-11 10:56:57 -0700

    Fixed prompt engineering

--- reply_query/flask_server.py
+++ reply_query/flask_server.py
@@ -26,24 +26,21 @@ def helloWorld():
 @app.route("/bedrock", methods=["POST"])
 def bedrock_query() -> dict:
     try:
-        print("pass")
         data = request.json
         string_query = data.get('query') # query needs to get transformed into a embedded text 
         embedding = text_embedding.embed_text(string_query) # transform for embed
-        print("pass2")
         matches = database_interactor.similarity_search(embedding)
-        # matches = data.get('matches')
-        print("pass match specifciation")
         client = boto3.client('bedrock-runtime', region_name='us-west-2')  
         query_text = (
             "Given the information of the initial query question that was asked and also the matching information "
-            "that was found with the text and that timeframe, provide the user with an answer to their question "
+            "that was found with the text and that timeframe, provide the user with an answer to their question or just reply to them as their assistant if they didn't ask a question,"
             "given the matches of information to their question"
+            "Also note that you are parrot.ai a tool that helps users remember information that was recorded."
         )
 
         formatted_matches = ' | '.join(' '.join(tup) for tup in matches)
         
-        context = string_query + query_text + formatted_matches
+        context = "Initial Query: " + string_query + "| Matches: " + formatted_matches + "| Context: "
         
         payload = {
             "modelId": "anthropic.claude-3-5-haiku-20241022-v1:0",
@@ -74,8 +71,6 @@ def bedrock_query() -> dict:
         return jsonify(response['output']['message']['content'][0]['text'])
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
